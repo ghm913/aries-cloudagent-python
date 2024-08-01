a3a98c045bb9c30d0f563c4eb34ee9f2883f8586
ghm913 <guohongming97@126.com>
2024-08-01 08:54:16 +0200

    final submit

---
+++ agent1_http3_ssl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+export PYTHONUNBUFFERED=1
+export ACAPY_ADMIN=[0.0.0.0,8021]
+export ACAPY_ADMIN_INSECURE_MODE=true
+export ACAPY_AUTO_ACCEPT_INVITES=true
+export ACAPY_AUTO_ACCEPT_REQUESTS=true
+export ACAPY_AUTO_PING_CONNECTION=true
+export ACAPY_AUTO_PROVISION=true
+export ACAPY_AUTO_RESPOND_MESSAGES=true
+export ACAPY_AUTO_STORE_CREDENTIAL=true
+export ACAPY_EMIT_NEW_DIDCOMM_PREFIX=true
+export ACAPY_ENDPOINT=https://172.28.56.159:8020
+export ACAPY_GENESIS_URL=http://test.bcovrin.vonx.io/genesis
+export ACAPY_LABEL=agent1_http3_ssl
+export ACAPY_OUTBOUND_TRANSPORT=http3
+export ACAPY_PRESERVE_EXCHANGE_RECORDS=true
+export ACAPY_PUBLIC_INVITES=true
+export ACAPY_REQUESTS_THROUGH_PUBLIC_DID=true
+export ACAPY_WALLET_KEY=secret
+export ACAPY_WALLET_NAME=anton
+export ACAPY_WALLET_SEED=anton000000000000000000000000000
+export ACAPY_WALLET_TYPE=askar
+export ENABLE_PROMETHEUS=true
+export PROMETHEUS_PORT=8022
+aca-py start \
+  --inbound-transport http3 0.0.0.0 8020 \
+  --log-level info \
+  --admin 0.0.0.0 8021 \
+  --admin-insecure-mode \
+  --auto-accept-invites \
+  --auto-accept-requests \
+  --auto-respond-messages \
+  --auto-store-credential \
+  --emit-new-didcomm-prefix \
+  --endpoint "https://172.28.56.159:8020" \
+  --genesis-url "http://test.bcovrin.vonx.io/genesis" \
+  --label "agent1_http3_ssl" \
+  --outbound-transport http3_ssl \
+  --preserve-exchange-records \
+  --public-invites \
+  --requests-through-public-did \
+  --wallet-key "secret" \
+  --wallet-name "anton" \
+  --wallet-type askar \
+  --seed "anton000000000000000000000000000"
---
+++ agent2_http1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+export PYTHONUNBUFFERED=1
+export ACAPY_ADMIN=[0.0.0.0,8031]
+export ACAPY_ADMIN_INSECURE_MODE=true
+export ACAPY_AUTO_ACCEPT_INVITES=true
+export ACAPY_AUTO_ACCEPT_REQUESTS=true
+export ACAPY_AUTO_PING_CONNECTION=true
+export ACAPY_AUTO_PROVISION=true
+export ACAPY_AUTO_RESPOND_MESSAGES=true
+export ACAPY_AUTO_STORE_CREDENTIAL=true
+export ACAPY_EMIT_NEW_DIDCOMM_PREFIX=true
+export ACAPY_ENDPOINT=http://172.28.56.159:8030
+export ACAPY_GENESIS_URL=http://test.bcovrin.vonx.io/genesis
+export ACAPY_LABEL=agent2_http1
+export ACAPY_OUTBOUND_TRANSPORT=http
+export ACAPY_PRESERVE_EXCHANGE_RECORDS=true
+export ACAPY_PUBLIC_INVITES=true
+export ACAPY_REQUESTS_THROUGH_PUBLIC_DID=true
+export ACAPY_WALLET_KEY=secret
+export ACAPY_WALLET_NAME=anton
+export ACAPY_WALLET_SEED=anton000000000000000000000000000
+export ACAPY_WALLET_TYPE=askar
+export ENABLE_PROMETHEUS=true
+export PROMETHEUS_PORT=8032
+aca-py start --inbound-transport http 0.0.0.0 8030 \
+              --log-level info \
+              --admin 0.0.0.0 8031 \
+              --admin-insecure-mode \
+              --auto-accept-invites \
+              --auto-accept-requests \
+              --auto-respond-messages \
+              --auto-store-credential \
+              --emit-new-didcomm-prefix \
+              --endpoint "http://172.28.56.159:8030" \
+              --genesis-url "http://test.bcovrin.vonx.io/genesis" \
+              --label "agent2_http1" \
+              --outbound-transport http \
+              --preserve-exchange-records \
+              --public-invites \
+              --requests-through-public-did \
+              --wallet-key "secret" \
+              --wallet-name "anton" \
+              --wallet-type askar \
+              --seed "anton000000000000000000000000000"
